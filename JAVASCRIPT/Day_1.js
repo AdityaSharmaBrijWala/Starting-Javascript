423c39b1a8e0b9a0aca4e13642564a8930ebfeff
AdityaSharmaBrijWala <168361334+AdityaSharmaBrijWala@users.noreply.github.com>
2024-04-29 11:43:08 +0000

    done until maths and num

--- JAVASCRIPT/Day_1.js
+++ JAVASCRIPT/Day_1.js
@@ -1,1 +1,273 @@
 // console.log("Hello World!");
+const accountId = 144553
+let accountEmail = "aditya@google.com"
+var accountPassword = "12345"
+accountCity = "Mathura"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "adi@google.com"
+accountPassword = "21212121"
+accountCity = "Noida"
+
+// console.log(accountId,accountEmail,accountPassword);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+// console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+
+
+
+// *****************************  VIDEO 2  *****************************
+
+
+
+
+
+
+/*
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+*/
+
+
+
+// *****************************  VIDEO 3  *****************************
+
+/*
+
+
+let score = "aditya"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "aditya"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+console.log(2%3);
+
+let str1 = "hello"
+let str2 = " aditya"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+*/
+
+
+
+// *****************************  VIDEO 5  *****************************
+
+
+
+
+/*
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
+
+
+*/
+
+
+
+// *****************************  VIDEO 6  *****************************
+
+
+/*
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+*/
+
+
+
+
+
+
+
+
+// *****************************  VIDEO 7  *****************************
+
+
+
+const name = "aditya sharma"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('aditya-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(0));
+console.log(gameName.indexOf('a'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   aditya    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://aditya.com/aditya%20sharma"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('aditya'))
+
+console.log(gameName.split('-'));
